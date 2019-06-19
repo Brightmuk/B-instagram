420e62cef57d30f372f0b058caa9639edaf33d2a
Brightmuk <be.rightmuk@gmail.com>
2019-06-19 14:02:59 +0300

    Create the view function for the homepage

--- instagram/views.py
+++ instagram/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
+import datetime as dt
+from django.http  import HttpResponse,Http404
+from django.shortcuts import render,redirect
 from .models import Image,Profile
 
-
+def home(request):
+    images = Image.objects.all()
+    
+    return render(request, 'index.html',{"images":images})
