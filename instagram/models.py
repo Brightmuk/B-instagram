8b59dc7f7b04061305938c550b7ab0074df6ba19
Brightmuk <be.rightmuk@gmail.com>
2019-06-19 17:51:38 +0300

    Create coment model tohold comments of a single photo

--- instagram/models.py
+++ instagram/models.py
@@ -5,17 +5,23 @@ class Profile(models.Model):
     bio = models.CharField(max_length =200)
     profile_pic_path = models.ImageField(upload_to = 'photos/', default='DEFAULT VALUE')
     user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True,default=1)
-    followers = models.ManyToManyField('Profile',related_name="user_followers",blank=True)
-    following = models.ManyToManyField('Profile',related_name="user_following",blank=True)
+    followers = models.ManyToManyField('Profile',related_name="profile_followers",blank=True)
+    following = models.ManyToManyField('Profile',related_name="profile_following",blank=True)
 
 class Image(models.Model):
     image = models.ImageField(upload_to = 'photos/', default='DEFAULT VALUE')
+    owner = models.ForeignKey(User, null=True, on_delete=models.CASCADE,related_name="user_name")
     profile = models.ForeignKey(Profile)
     image_name = models.CharField(max_length =30)
-    comments = models.CharField(max_length =200,blank =True)
     caption = models.CharField(max_length =50)
     post_date = models.DateTimeField(auto_now_add=True)
     likes = models.IntegerField(default=0)
      
     def __str__(self):
         return self.image_name
+
+class Comment(models.Model):
+    image = models.ForeignKey('Image', null=True)
+    user = models.ForeignKey(User)
+    comment = models.CharField(max_length=100)
+    posted_on = models.DateTimeField(auto_now=True)
