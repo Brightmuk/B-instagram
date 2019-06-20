c2682cbe1a62fc0af86c6bb4b27cc871f5d47ef3
Brightmuk <be.rightmuk@gmail.com>
2019-06-20 09:11:08 +0300

    Create the view function to direct a user to their profile page

--- instagram/views.py
+++ instagram/views.py
@@ -4,10 +4,16 @@ import datetime as dt
 from django.http  import HttpResponse,Http404
 from django.shortcuts import render,redirect
 from .models import Image,Profile
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/accounts/login/')
 def home(request):
     images = Image.objects.all()
-    
-    return render(request, 'index.html',{"images":images})
+    users = User.objects.all()
+    return render(request, 'index.html',{"images":images,'users':users})
+
+def profile(request,id):
+    user = User.objects.filter(id=id)
+
+    return render(request,'profile/profile.html',{'user':user})
